58e1404bd8ef69fb65d5e60150cfa3e5cc5ef9e8
BeauBrewski <85623381+BeauBrewski@users.noreply.github.com>
2023-07-15 07:24:52 -0500

    Update target.h

--- src/main/target/SKYSTARSF405AIO/target.h
+++ src/main/target/SKYSTARSF405AIO/target.h
@@ -30,7 +30,7 @@
 //#define USE_BARO_SPI_BMP280
 #define USE_FLASHFS
 #define USE_FLASH_W25Q128FV
-#define USE_MAX7456
+//#define USE_MAX7456
 #define USE_BARO
 #define USE_BARO_BMP280
 #define USE_ADC
@@ -60,7 +60,7 @@
 #define SPI1_SCK_PIN         PA5
 #define SPI1_MOSI_PIN        PA6
 #define SPI1_MISO_PIN        PA7
-#define USE_SPI_DEVICE_2  //baro/max7456
+//#define USE_SPI_DEVICE_2  //baro/max7456
 //#define SPI2_SCK_PIN         PB13
 //#define SPI2_MOSI_PIN        PB14
 //#define SPI2_MISO_PIN        PB15
